3d5b324fcf97db6348550eb7df0b48bba5278753
Jorgen Schaefer <contact@jorgenschaefer.de>
2016-10-31 20:51:16 +0100

    Meh

--- bin/ci-test.sh
+++ bin/ci-test.sh
@@ -7,4 +7,5 @@ export GOPATH="$(pwd)/gopath"
 mkdir -p "$GOPATH/src/github.com/jorgenschaefer"
 ln -s "$(pwd)/journalcheck-source" "$GOPATH/src/github.com/jorgenschaefer/"
 
-go get 
+cd journalcheck-source/
+go get .
