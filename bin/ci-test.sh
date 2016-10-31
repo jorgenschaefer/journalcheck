7197b2ef78e1768768591d7f409a2eb471d8baa3
Jorgen Schaefer <contact@jorgenschaefer.de>
2016-10-31 21:08:20 +0100

    Meh

--- bin/ci-test.sh
+++ bin/ci-test.sh
@@ -4,15 +4,9 @@ set -e
 
 export GOPATH="$(pwd)/gopath"
 
-echo '***'
-id
-pwd
-which apt-get
-echo '***'
-
 cd "$(dirname "$0")"/..
 
-sudo apt-get install libsystemd-dev
+apt-get install libsystemd-dev
 
 go get .
 go test ./...
