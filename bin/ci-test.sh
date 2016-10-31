ec5acf70d420527fbc8d8b2bdded1a5c1cb6da77
Jorgen Schaefer <contact@jorgenschaefer.de>
2016-10-31 20:47:28 +0100

    Test initial scripts

---
+++ bin/ci-test.sh
@@ -0,0 +1,10 @@
+#!/bin/ci
+
+cd "$(dirname "$0")"/../..
+
+mkdir gopath
+export GOPATH="$(pwd)/gopath"
+mkdir -p "$GOPATH/src/github.com/jorgenschaefer"
+ln -s "$(pwd)/journalcheck-source" "$GOPATH/src/github.com/jorgenschaefer/"
+
+go get 
